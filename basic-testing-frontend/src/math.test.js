09446b24c9455481ca9a22ff4958234b546f296d
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-02-26 21:26:00 +0000

    create first test

---
+++ basic-testing-frontend/src/math.test.js
@@ -0,0 +1,14 @@
+import { it, expect } from 'vitest';
+import { add } from './math';
+
+it('should summarize all number values in an array', () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = 6;
+  expect(result).toBe(expectedResult);
+});
