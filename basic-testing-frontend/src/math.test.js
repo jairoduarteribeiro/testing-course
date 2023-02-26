9edc32d5ccc7fdb5044f59f24388174d5a0390bf
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-02-26 21:41:42 +0000

    accept an array of strings

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -12,3 +12,26 @@ it('should summarize all number values in an array', () => {
   const expectedResult = 6;
   expect(result).toBe(expectedResult);
 });
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  // Arrange
+  const numbers = ['1', '2', '3'];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = 6;
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+  // Arrange
+  const numbers = [1, 'invalid', 3];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBeNaN();
+});
