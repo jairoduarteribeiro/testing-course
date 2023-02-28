0d43bbe20b2b539d451beb1567fc9d4c476c258d
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-02-28 23:10:49 +0000

    replicate tests to other projects

---
+++ basic-testing-nodejs-esmodules/src/math.test.js
@@ -0,0 +1,72 @@
+import { it, expect, describe } from 'vitest';
+import { add } from './math';
+
+describe('add()', () => {
+  it('should summarize all number values in an array', () => {
+    // Arrange
+    const numbers = [1, 2, 3];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = 6;
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should yield a correct sum if an array of numeric string values is provided', () => {
+    // Arrange
+    const numbers = ['1', '2', '3'];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = 6;
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should yield NaN if at least one invalid number is provided', () => {
+    // Arrange
+    const numbers = [1, 'invalid', 3];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBeNaN();
+  });
+
+  it('should yield 0 if an empty array is provided', () => {
+    // Arrange
+    const numbers = [];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = 0;
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should throw if no value is provided', () => {
+    // Arrange
+    const fn = function () {
+      add();
+    };
+
+    // Assert
+    expect(fn).toThrow(/is not iterable/);
+    expect(() => add()).toThrow(/is not iterable/);
+  });
+
+  it('should throw if multiple values are provided', () => {
+    // Arrange
+    const number1 = 1;
+    const number2 = 2;
+    const number3 = 3;
+
+    // Assert
+    expect(() => add(number1, number2, number3)).toThrow(/is not iterable/);
+  });
+});
---
+++ basic-testing-nodejs/src/util/numbers.test.js
@@ -0,0 +1,30 @@
+import { it, expect, describe } from 'vitest';
+import { transformToNumber } from './numbers';
+
+describe('transformToNumber()', () => {
+  it('should transform a numeric text to a number', () => {
+    // Arrange
+    const number = '3';
+
+    // Act
+    const result = transformToNumber(number);
+
+    // Assert
+    expect(result).toBeTypeOf('number');
+    expect(result).toBe(3);
+  });
+
+  it('should yield NaN when trying to transform a non-numeric value', () => {
+    // Arrange
+    const number1 = 'invalid';
+    const number2 = {};
+
+    // Act
+    const result1 = transformToNumber(number1);
+    const result2 = transformToNumber(number2);
+
+    // Assert
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+  });
+});
